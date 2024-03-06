f5469f72800e55c7ea1266b4d84b7251b576290a
katherine Gómez <kaligocas1990@gmail.com>
2024-03-06 21:59:17 +0100

    created model

--- src/main/java/com/delivery_food/model/CartItem.java
+++ src/main/java/com/delivery_food/model/CartItem.java
@@ -1,31 +1,33 @@
 package com.delivery_food.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-
-public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+@AllArgsConstructor
+@Entity
+public class CartItem {
     private Long id;
-
-    private String name;
 
     @ManyToOne
     @JsonIgnore
-    private Restaurant restaurant;
+    private Cart cart;
 
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private List<String> ingredients;
+
+    private Long totalPrice;
 
 }
--- src/main/java/com/delivery_food/model/Order.java
+++ src/main/java/com/delivery_food/model/Order.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "orders")
 public class Order {
 
     @Id
